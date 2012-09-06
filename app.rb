709cab316706ac655accd5148b334ef8e1ab6c01
ctruman <cleetruman@gmail.com>
2012-09-06 13:15:03 -0700

    adding radius limit

--- app.rb
+++ app.rb
@@ -44,7 +44,8 @@ CATEGORY= {
 post '/category' do
   params = JSON.parse(request.body.read)
 #   response = findLocal(CATEGORY[params['category'].to_sym][:category], params['location'], CATEGORY[params['category'].to_sym][:distance], 4)
-	response = CityGridApi::LatLonSearch.find({:tag => CATEGORY[params['category'].to_sym][:tag], :lat => params["location"][0], :lon => params["location"][1], :rpp => 4, :page => 1, :sort => "highestrated", :radius => 2})
+# :tag => CATEGORY[params['category'].to_sym][:tag]
+	response = CityGridApi::LatLonSearch.find({:what => CATEGORY[params['category'], :lat => params["location"][0], :lon => params["location"][1], :rpp => 4, :page => 1, :sort => "highestrated", :radius => 2})
   json(response['results']['locations'], :encoder => :to_json)
 end
 
